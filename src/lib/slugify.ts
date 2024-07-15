48593cf9763cc95973cd2b052678f4c7e8b3eaed
jaganoer-works <jaganoer@gmail.com>
2024-07-15 16:13:18 +0900

    ブログ投稿ページを削除

--- src/lib/slugify.ts
+++
@@ -1,9 +0,0 @@
-export function slugify(text: string): string {
-  return text
-    .toString()
-    .toLowerCase()
-    .trim()
-    .replace(/\s+/g, "-") // スペースをハイフンに置換
-    .replace(/[^\w\-]+/g, "") // 英数字とハイフン以外を削除
-    .replace(/\-\-+/g, "-"); // 連続したハイフンを単一のハイフンに置換
-}
